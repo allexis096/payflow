eeeb1d990c09551851d0ab2a40e008bddf010c56
allexis096 <allexissoares@gmail.com>
2021-06-27 14:28:54 -0300

    :art: chore: update configs and theme

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
 import 'shared/themes/appcolors.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pay Flow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
+      theme: ThemeData(primaryColor: AppColors.primary),
       home: LoginPage(),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashPage(),
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
