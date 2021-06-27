000e742b9c3d5de350fe7bbabccfb2e523123a64
allexis096 <allexissoares@gmail.com>
2021-06-27 14:30:21 -0300

    :sparkles: feat: add home_controller, auth_controller and user_model

---
+++ lib/shared/auth/auth_controller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthController {
+  UserModel? _user;
+
+  UserModel get getUser => _user!;
+
+  void setUser(BuildContext context, UserModel? user) {
+    if (user != null) {
+      _user = user;
+      saveUser(user);
+
+      Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+    } else {
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+
+    await instance.setString('user', user.toJson());
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    await Future.delayed(Duration(seconds: 2));
+
+    if (instance.containsKey('user')) {
+      final json = instance.get('user') as String;
+      return setUser(context, UserModel.fromJson(json));
+    }
+
+    setUser(context, null);
+  }
+}
